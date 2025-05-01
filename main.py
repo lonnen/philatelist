de2e23d990d9a64f296f2f5c7a3c07545cc5f510
lonnen <chris.lonnen@gmail.com>
2025-04-30 23:10:24 -0700

    we're gonna need a username, and probably a token
    the token is not best practice but that's just like, your opinion, man

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from philatelist!")
+import argparse
+import os
 
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="collect a single user's commits from GitHub"
+    )
+    parser.add_argument("username", help="GitHub username of interest")
+    parser.add_argument(
+        "token",
+        nargs="?",
+        const=os.environ.get("GITHUB_TOKEN"),
+        default=os.environ.get("GITHUB_TOKEN"),
+        help="""(optional) GitHub personal access token - see:
+        https://docs.github.com/en/rest/authentication/authenticating-to-the-rest-api""",
+    )
+
+    args = parser.parse_args()
+
+    if not args.token:
+        print("WARNING: No GitHub Token. Rate limiting may occur.")
+        print("Re-run with --help for more information")
+
+    print(args.username)
+    print(args.token)
 
 if __name__ == "__main__":
     main()

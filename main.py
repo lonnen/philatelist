6246aa18610820e0a23403d48ee5c4077a27aabc
lonnen <chris.lonnen@gmail.com>
2025-05-01 00:04:20 -0700

    add in all the scaffolding necessary to make a request
    Github returns a 403 reporting we're over the rate limit on the initial request. Gross. On the upside, the error handling and reporting works well enough for debugging

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import argparse
+import json
 import os
 
+import requests
+
+GITHUB_API_URL = "https://api.github.com/graphql"
 
 def main():
     parser = argparse.ArgumentParser(
@@ -18,12 +22,37 @@ def main():
 
     args = parser.parse_args()
 
-    if not args.token:
+    username = args.username
+    token = args.token
+    if not token:
         print("WARNING: No GitHub Token. Rate limiting may occur.")
         print("Re-run with --help for more information")
 
-    print(args.username)
-    print(args.token)
+    headers = {
+        "Content-Type": "application/json",
+        "User-Agent": "philatelist"
+    }
+
+    if token:
+        headers["Authorization"] = f"Bearer {token}"
+
+    response = requests.post(
+        GITHUB_API_URL,
+        headers=headers,
+        json={
+            "query": "",
+            "variables": ""
+        }
+    )
+
+    if response.status_code == 200:
+        return response.json()
+
+    try:
+        error_message = json.dumps(response.json(), indent=2)
+    except:
+        error_message = response.text
+    raise Exception(f"GitHub API status {response.status_code}: {error_message}")
 
 if __name__ == "__main__":
     main()
